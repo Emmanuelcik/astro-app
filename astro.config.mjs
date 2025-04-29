60d764010abcc0c664c7d64cbc5a80a700e53dd7
Emmmanuel Lopez <jesusemmcik@gmail.com>
2025-04-29 00:00:53 -0600

    added buy botton and view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, envField } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      SCORE_API_ENDPOINT: envField.string({
+        context: "client",
+        access: "public",
+      }),
+    },
+  },
 });
---
+++ env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+  readonly PUBLIC_SHOW_BUY_BUTTON: string;
+  readonly SCORE_API_ENDPOINT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
